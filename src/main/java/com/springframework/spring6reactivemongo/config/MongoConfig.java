2e50cd3a26820f95485a7387fa0eef8848998ce5
serchg0george <serchg0george@gmail.com>
2023-10-14 20:52:34 +0300

    Add config to client settings

--- src/main/java/com/springframework/spring6reactivemongo/config/MongoConfig.java
+++ src/main/java/com/springframework/spring6reactivemongo/config/MongoConfig.java
@@ -1,10 +1,15 @@
 package com.springframework.spring6reactivemongo.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+import java.util.Collections;
 
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
@@ -17,5 +22,16 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     @Override
     protected String getDatabaseName() {
         return "sfg";
+    }
+
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder.credential(MongoCredential.createCredential("root",
+                "admin", "example".toCharArray()))
+                .applyToClusterSettings(settings -> {
+                    settings.hosts((Collections.singletonList(
+                            new ServerAddress("127.0.0.1", 27017)
+                    )));
+                });
     }
 }
