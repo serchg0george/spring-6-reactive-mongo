77c1aade9bf4fb3321d1256b671b2769ac2beaac
serchg0george <serchg0george@gmail.com>
2023-10-14 20:22:58 +0300

    Create entity and DTO for beer

---
+++ src/main/java/com/springframework/spring6reactivemongo/model/BeerDTO.java
@@ -0,0 +1,36 @@
+package com.springframework.spring6reactivemongo.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDTO {
+
+    private Integer id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
+    private String beerName;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
+    private String beerStyle;
+
+    @Size(max = 25)
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModified;
+
+}
