be99c2f8316c0a6eebe23156b49670a1932c9e34
serchg0george <serchg0george@gmail.com>
2023-10-14 20:08:02 +0300

    Initial commit

---
+++ src/test/java/com/springframework/spring6reactivemongo/Spring6ReactiveMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.spring6reactivemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
