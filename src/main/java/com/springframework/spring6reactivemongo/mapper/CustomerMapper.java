a7eead160fd5603103b97444c50edea5a4c6c2f2
serchg0george <serchg0george@gmail.com>
2023-10-15 15:54:32 +0300

    Remove unused imports

--- src/main/java/com/springframework/spring6reactivemongo/mapper/CustomerMapper.java
+++ src/main/java/com/springframework/spring6reactivemongo/mapper/CustomerMapper.java
@@ -1,8 +1,6 @@
 package com.springframework.spring6reactivemongo.mapper;
 
-import com.springframework.spring6reactivemongo.domain.Beer;
 import com.springframework.spring6reactivemongo.domain.Customer;
-import com.springframework.spring6reactivemongo.model.BeerDTO;
 import com.springframework.spring6reactivemongo.model.CustomerDTO;
 import org.mapstruct.Mapper;
 
