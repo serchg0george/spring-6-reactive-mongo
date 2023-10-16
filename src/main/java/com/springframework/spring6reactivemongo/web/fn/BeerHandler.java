43cb698a83b9d4e980cde19ec1783d7137306579
serchg0george <serchg0george@gmail.com>
2023-10-16 15:05:01 +0300

    Added webflux handler

---
+++ src/main/java/com/springframework/spring6reactivemongo/web/fn/BeerHandler.java
@@ -0,0 +1,20 @@
+package com.springframework.spring6reactivemongo.web.fn;
+
+import com.springframework.spring6reactivemongo.model.BeerDTO;
+import com.springframework.spring6reactivemongo.service.BeerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class BeerHandler {
+    private final BeerService beerService;
+
+    public Mono<ServerResponse> listBeer(ServerRequest request) {
+        return ServerResponse.ok()
+                .body(beerService.listBeers(), BeerDTO.class);
+    }
+}
