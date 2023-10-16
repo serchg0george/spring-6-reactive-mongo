fda9708892c1bfcac8ef42c00013406130e869de
serchg0george <serchg0george@gmail.com>
2023-10-16 18:23:45 +0300

    Added router config

---
+++ src/main/java/com/springframework/spring6reactivemongo/web/fn/CustomerRouterConfig.java
@@ -0,0 +1,27 @@
+package com.springframework.spring6reactivemongo.web.fn;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+@RequiredArgsConstructor
+public class CustomerRouterConfig {
+    public static final String CUSTOMER_PATH = "/api/v3/customer";
+    public static final String CUSTOMER_PATH_ID = CUSTOMER_PATH + "/{customerId}";
+
+    private final CustomerHandler customerHandler;
+
+    @Bean
+    public RouterFunction<ServerResponse> customerRoutes() {
+        return route().
+                GET(CUSTOMER_PATH, accept(APPLICATION_JSON), customerHandler::listCustomer)
+                .build();
+    }
+}
